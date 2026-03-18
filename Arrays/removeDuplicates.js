bbdfd0794bd674c88b3549693d16d24650a7a887
RohanNaidu97 <rohannaidu97@gmail.com>
2026-03-18 16:36:49 +0530

    [Arrays]: Remove Elements

--- Arrays/removeDuplicates.js
+++ Arrays/removeDuplicates.js
@@ -11,3 +11,18 @@ var removeDuplicates = function (nums) {
 };
 
 console.log(removeDuplicates(n));
+
+
+
+//The Video's approach
+var removeDuplicates = function(nums) {
+    let x = 0;
+    for(let i =0; i<nums.length; i++){
+        if(nums[i] > nums[x]){
+            x++;
+            nums[x] = nums[i];
+        }
+    }
+
+    return (x+1);
+};
